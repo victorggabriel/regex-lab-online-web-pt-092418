de330c033bedb3635689386d160e3f38b34cbefa
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-10-11 05:12:02 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[AEIOUaeiou]/)
+      true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+   text.match? /^[A-Z].*\W$/
 end
 
 def valid_phone_number?(phone)
-
+   phone.match? /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
 end
