221ea5648629831abcdecce1d416e7f3d7f236a0
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-10-12 20:54:53 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,9 +11,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-   text.match(/^[A-Z].*\W$/)
+   text.match? /^[A-Z].*\W$/
 end
 
 def valid_phone_number?(phone)
-   phone.match? /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
+   phone.match /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
 end
