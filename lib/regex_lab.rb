65fb868b6694f134770f725ae921260596a92f51
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-10-12 20:58:51 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  word.match(/^[AEIOUaeiou]/)
+  /^[AEIOUaeiou]/.match?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -11,7 +11,6 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
    /^[A-Z].*\W$/.match?(text)
 end
 
